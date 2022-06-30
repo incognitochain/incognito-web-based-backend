d282608707818edd91df5ce719369e61063a4a22
0xBahamoot <lam@incognito.org>
2022-06-30 14:27:35 +0700

    update api

--- api/address.go
+++ api/address.go
@@ -42,3 +42,38 @@ func APIGenUnshieldAddress(c *gin.Context) {
 		return
 	}
 }
+
+func APIGenShieldAddress(c *gin.Context) {
+	var req GenShieldAddressRequest
+	err := c.MustBindWith(&req, binding.JSON)
+	if err != nil {
+		c.JSON(400, gin.H{"Error": err.Error()})
+		return
+	}
+
+	switch req.Network {
+	case "eth", "bsc", "plg", "ftm":
+		re, err := restyClient.R().
+			EnableTrace().
+			SetHeader("Content-Type", "application/json").SetHeader("Authorization", "Bearer "+usa.token).SetBody(req).
+			Post(config.ShieldService + "/" + req.Network + "/generate")
+		if err != nil {
+			c.JSON(400, gin.H{"Error": err.Error()})
+			return
+		}
+		var responseBodyData struct {
+			Result interface{}
+			Error  interface{}
+		}
+		err = json.Unmarshal(re.Body(), &responseBodyData)
+		if err != nil {
+			c.JSON(400, gin.H{"Error": err})
+			return
+		}
+		c.JSON(200, responseBodyData)
+		return
+	default:
+		c.JSON(400, gin.H{"Error": errors.New("unsupport network")})
+		return
+	}
+}

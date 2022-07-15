3f24fef0d044be6775d0c160fe1a88869542dbef
0xBahamoot <lam@incognito.org>
2022-07-15 18:22:50 +0700

    wip

--- common/constants.go
+++ common/constants.go
@@ -2,7 +2,7 @@ package common
 
 var DefaultConfig = Config{
 	Port:           9898,
-	DatabaseURL:    "mongodb://localhost:27017",
+	DatabaseURLs:   []string{"127.0.0.1:6379"},
 	CoinserviceURL: "http://51.161.117.193:8096",
 	FullnodeURL:    "https://testnet.incognito.org/fullnode",
 	ShieldService:  "https://staging-api-service.incognito.org",

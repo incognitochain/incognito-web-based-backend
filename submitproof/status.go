24952f66c9cfd4a2f04eb6b24820f643e28a2fce
0xBahamoot <lam@incognito.org>
2022-07-18 17:00:16 +0700

    update

--- submitproof/status.go
+++ submitproof/status.go
@@ -3,13 +3,18 @@ package submitproof
 import (
 	"context"
 	"fmt"
+
+	"github.com/rueian/rueidis"
 )
 
 func getShieldTxStatus(externalTxHash string, networkID int, tokenID string) (int, error) {
 	ctx := context.Background()
-	key := buildShieldStatusKey(externalTxHash, networkID, tokenID)
+	key := ShieldStatusPrefix + buildShieldStatusKey(externalTxHash, networkID, tokenID)
 	value, err := db.Do(ctx, db.B().Get().Key(key).Build()).AsInt64()
 	if err != nil {
+		if err.Error() == "redis nil message" {
+			return 0, nil
+		}
 		return 0, err
 	}
 	return int(value), nil
@@ -18,15 +23,25 @@ func getShieldTxStatus(externalTxHash string, networkID int, tokenID string) (in
 func updateShieldTxStatus(externalTxHash string, networkID int, tokenID string, status int) error {
 	ctx := context.Background()
 
-	key := buildShieldStatusKey(externalTxHash, networkID, tokenID)
+	key := ShieldStatusPrefix + buildShieldStatusKey(externalTxHash, networkID, tokenID)
 	value := fmt.Sprint(status)
-	return db.Do(ctx, db.B().Set().Key(key).Value(value).Nx().Build()).Error()
+	action := func(rd rueidis.DedicatedClient) error {
+		result := rd.DoMulti(ctx, db.B().Set().Key(key).Value(value).Build(), rd.B().Persist().Key(key).Build())
+		for _, rs := range result {
+			if rs.RedisError() != nil {
+				return rs.RedisError()
+			}
+		}
+		return nil
+	}
+
+	return db.Dedicated(action)
 }
 
 func setShieldTxStatusError(externalTxHash string, networkID int, tokenID string, errStr string) error {
 	ctx := context.Background()
 
-	key := buildShieldStatusKey(externalTxHash, networkID, tokenID)
+	key := ShieldErrorPrefix + buildShieldStatusKey(externalTxHash, networkID, tokenID)
 	value := fmt.Sprint(errStr)
 	return db.Do(ctx, db.B().Set().Key(key).Value(value).Nx().Build()).Error()
 }
--- submitproof/worker.go
+++ submitproof/worker.go
@@ -1,12 +1,17 @@
 package submitproof
 
 import (
-	"github.com/RichardKnop/machinery/v1/log"
+	"log"
+
 	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/incognitochain/go-incognito-sdk-v2/incclient"
+	"github.com/incognitochain/go-incognito-sdk-v2/wallet"
+	wcommon "github.com/incognitochain/incognito-web-based-backend/common"
+	"github.com/pkg/errors"
 )
 
 func worker() error {
-	consumerTag := "machinery_worker"
+	consumerTag := "submitproof_worker"
 
 	// cleanup, err := tracers.SetupTracer(consumerTag)
 	// if err != nil {
@@ -26,15 +31,15 @@ func worker() error {
 	// Here we inject some custom code for error handling,
 	// start and end of task hooks, useful for metrics for example.
 	errorhandler := func(err error) {
-		log.ERROR.Println("I am an error handler:", err)
+		log.Println("I am an error handler:", err)
 	}
 
 	pretaskhandler := func(signature *tasks.Signature) {
-		log.INFO.Println("I am a start of task handler for:", signature.Name)
+		log.Println("I am a start of task handler for:", signature.Name)
 	}
 
 	posttaskhandler := func(signature *tasks.Signature) {
-		log.INFO.Println("I am an end of task handler for:", signature.Name)
+		log.Println("I am an end of task handler for:", signature.Name)
 	}
 
 	worker.SetPostTaskHandler(posttaskhandler)
@@ -42,4 +47,43 @@ func worker() error {
 	worker.SetPreTaskHandler(pretaskhandler)
 
 	return worker.Launch()
+}
+
+func StartWorker(keylist []string, network string, cfg wcommon.Config) error {
+	config = cfg
+	keyList = keylist
+
+	err := connectDB(cfg.DatabaseURLs)
+	if err != nil {
+		return err
+	}
+
+	switch network {
+	case "mainnet":
+		incClient, err = incclient.NewMainNetClient()
+	case "testnet": // testnet2
+		incClient, err = incclient.NewTestNetClient()
+	case "testnet1":
+		incClient, err = incclient.NewTestNet1Client()
+	case "devnet":
+		return errors.New("unsupported network")
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, v := range keyList {
+		wl, err := wallet.Base58CheckDeserialize(v)
+		if err != nil {
+			panic(err)
+		}
+		err = incClient.SubmitKey(wl.Base58CheckSerialize(wallet.OTAKeyType))
+		if err != nil {
+			return err
+		}
+	}
+	incclient.Logger = incclient.NewLogger(true)
+	log.Println("Done submit keys")
+
+	return nil
 }

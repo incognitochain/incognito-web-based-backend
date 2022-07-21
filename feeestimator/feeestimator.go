96c8a429c8cb68eb3e86564f669aad0c7818fc76
0xBahamoot <lam@incognito.org>
2022-07-21 11:01:41 +0700

    wip

---
+++ feeestimator/feeestimator.go
@@ -0,0 +1,19 @@
+package feeestimator
+
+import "github.com/incognitochain/incognito-web-based-backend/common"
+
+func StartService(network string, cfg common.Config) error {
+
+	return checkFee()
+}
+
+func checkFee() error {
+	for {
+
+	}
+	return nil
+}
+
+func getFee(network string) error {
+	return nil
+}

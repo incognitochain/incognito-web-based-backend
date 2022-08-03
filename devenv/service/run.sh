eaec8c50193e22926ec69e691729527c1e49a3ec
Incognito Developer <develop@incognito.org>
2022-08-03 10:13:22 +0700

    update Docker

--- devenv/service/run.sh
+++ devenv/service/run.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
-REDISHOST="$(getent hosts redisstack | awk '{ print $1 }')"
+REDISHOST_FROM_HOSTNAME="$(getent hosts redisstack | awk '{ print $1 }')"
+REDISHOST="${$REDISHOST:-REDISHOST_FROM_HOSTNAME}"
 
-JSON='{"Port":'"$Port"',"NetworkID": "'"$NetworkID"'","Mode":"'"$MODE"'","DatabaseURLs":["'"$REDISHOST"':6379"],"DBUSER":"'"$DBUser"'","DBPASS":"'"$DBPass"'","CoinserviceURL":"'"$CoinserviceURL"'","FullnodeURL":"'"$FullnodeURL"'","ShieldService":"'"$ShieldService"'","CaptchaSecret":"'"$CaptchaSecret"'"}'
-echo $JSON
-echo $JSON > cfg.json
-./webservice
+CONFIG_JSON=$(cat <<EOF
+{
+  "Port": $Port,
+  "NetworkID": "$NetworkID",
+  "Mode": "$MODE",
+  "Database":["$REDISHOST:6379"],
+  "DBUSER":"$DBUser",
+  "DBPASS":"$DBPass",
+  "CoinserviceURL": "$CoinserviceURL",
+  "FullnodeURL": "$FullnodeURL",
+  "ShieldService": "$ShieldService",
+  "FaucetService": "$FaucetService",
+  "CaptchaSecret":"$CaptchaSecret"
+}
+EOF
+)
+
+echo $CONFIG_JSON
+printf "$CONFIG_JSON" > cfg.json
+
+# "$@" to pass through all arguments to script
+./webservice "$@"

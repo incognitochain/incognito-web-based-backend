6b0553e90b746e3f111707f9bc1cbc1ab84cc8b2
Yen Le <levu.daiyen@gmail.com>
2022-08-03 10:35:45 +0700

    update env file

--- devenv/service/run.sh
+++ devenv/service/run.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 REDISHOST_FROM_HOSTNAME="$(getent hosts redisstack | awk '{ print $1 }')"
-REDISHOST="${$REDISHOST:-REDISHOST_FROM_HOSTNAME}"
+REDIS_HOST="${REDIS_HOST:-$REDISHOST_FROM_HOSTNAME}"
+REDIS_PORT="${$REDIS_PORT:-6379}"
 
 CONFIG_JSON=$(cat <<EOF
 {
-  "Port": $Port,
-  "NetworkID": "$NetworkID",
+  "Port": $PORT,
+  "NetworkID": "$NETWORK_ID",
   "Mode": "$MODE",
-  "Database":["$REDISHOST:6379"],
-  "DBUSER":"$DBUser",
-  "DBPASS":"$DBPass",
-  "CoinserviceURL": "$CoinserviceURL",
-  "FullnodeURL": "$FullnodeURL",
-  "ShieldService": "$ShieldService",
-  "FaucetService": "$FaucetService",
-  "CaptchaSecret":"$CaptchaSecret"
+  "Database":["$REDISHOST:$REDIS_PORT"],
+  "DBUSER":"$DB_USER",
+  "DBPASS":"$DB_PASSWORD",
+  "CoinserviceURL": "$COIN_SERVICE_URL",
+  "FullnodeURL": "$FULLNODE_URL",
+  "ShieldService": "$SHIELD_SERVICE_URL",
+  "FaucetService": "$FAUCET_SERVICE_URL",
+  "CaptchaSecret":"$CAPTCHA_SECRET"
 }
 EOF
 )

6ba4147f972067a691839e1aa072230570926289
Yen Le <levu.daiyen@gmail.com>
2022-08-03 10:50:45 +0700

    fix syntax

--- devenv/service/run.sh
+++ devenv/service/run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 REDISHOST_FROM_HOSTNAME="$(getent hosts redisstack | awk '{ print $1 }')"
 REDIS_HOST="${REDIS_HOST:-$REDISHOST_FROM_HOSTNAME}"
-REDIS_PORT="${$REDIS_PORT:-6379}"
+REDIS_PORT="${REDIS_PORT:-6379}"
 
 CONFIG_JSON=$(cat <<EOF
 {
   "Port": $PORT,
   "NetworkID": "$NETWORK_ID",
   "Mode": "$MODE",
-  "Database":["$REDISHOST:$REDIS_PORT"],
+  "Database":["$REDIS_HOST:$REDIS_PORT"],
   "DBUSER":"$DB_USER",
   "DBPASS":"$DB_PASSWORD",
   "CoinserviceURL": "$COIN_SERVICE_URL",

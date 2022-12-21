6b32e5d6e7c4366292e168436fe423fe9769f405
Hoang Phuong <63490227+hoangphuong24984@users.noreply.github.com>
2022-12-21 17:27:34 +0700

    update request create proposal: submit rawdata

--- api/pdao.go
+++ api/pdao.go
@@ -14,9 +14,12 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gin-gonic/gin"
 	"github.com/incognitochain/bridge-eth/common/base58"
+	"github.com/incognitochain/go-incognito-sdk-v2/metadata/bridge"
+	wcommon "github.com/incognitochain/incognito-web-based-backend/common"
 	"github.com/incognitochain/incognito-web-based-backend/database"
 	"github.com/incognitochain/incognito-web-based-backend/pdao/governance"
 	"github.com/incognitochain/incognito-web-based-backend/pdao/prvvote"
+	"github.com/incognitochain/incognito-web-based-backend/submitproof"
 )
 
 const GOVERNANCE_CONTRACT_ADDRESS = "0x74E9a67bf51eaa27999d8D699d3Ae4bAdc8c2Af4"
@@ -25,31 +28,8 @@ const PRV_THRESHOLD = "10000000000"
 
 func CreateNewProposal(c *gin.Context) {
 	var req CreatProposal
-	//userAgent := c.Request.UserAgent()
+	userAgent := c.Request.UserAgent()
 	err := c.ShouldBindJSON(&req)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
-		return
-	}
-
-	// verify request: request no empty, burn tx must be valid
-	// if len(req.Calldatas) != len(req.Targets) || len(req.Targets) != len(req.Values) || len(req.Targets) == 0 ||
-	// 	len(common.Hex2Bytes(req.Signature)) != 65 {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid proposal data"})
-	// 	return
-	// }
-
-	rawTxBytes, _, err := base58.Base58Check{}.Decode(req.TxRaw)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": errors.New("invalid txhash")})
-		return
-	}
-
-	// extract metadata from tx
-	// two outputs: 1 burn prv (optional) - 1 pay fee
-	// validate fee
-	// todo: lam update
-	_, _, _, _, err = extractDataFromRawTx(rawTxBytes)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
 		return
@@ -97,7 +77,7 @@ func CreateNewProposal(c *gin.Context) {
 
 	// recover address from user's signature
 	gvAbi, _ := abi.JSON(strings.NewReader(governance.GovernanceMetaData.ABI))
-	propEncode, _ := gvAbi.Pack("BuildSignProposalEncodeAbi", keccak256([]byte("proposal")), req.Targets, req.Values, req.Calldatas, req.Description)
+	propEncode, _ := gvAbi.Pack("BuildSignProposalEncodeAbi", keccak256([]byte("proposal")), req.Targets, req.Values, req.Calldatas, req.DescriptionLink)
 	signData, _ := gv.GetDataSign(nil, keccak256(propEncode[4:]))
 	rcAddr, err := crypto.Ecrecover(signData[:], common.Hex2Bytes(req.Signature))
 	// todo: compare address recover and address from burning metadata if has
@@ -155,13 +135,57 @@ func CreateNewProposal(c *gin.Context) {
 	// todo: submit transaction to CS and update to DB to get next step
 	//burntAmount, _ := md.TotalBurningAmount()
 	//if valid {
-	//	status, err := submitproof.SubmitPappTx(txHash, []byte(req.TxRaw), isPRVTx, feeToken, feeAmount, pfeeAmount, md.BurnTokenID.String(), burntAmount, swapInfo, isUnifiedToken, networkList, req.FeeRefundOTA, req.FeeRefundAddress, userAgent)
-	//	if err != nil {
-	//		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
-	//		return
-	//	}
-	//	c.JSON(200, gin.H{"Result": map[string]interface{}{"inc_request_tx_status": status}, "feeDiff": feeDiff})
-	//	return
+
+	rawTxBytes, _, err := base58.Base58Check{}.Decode(req.TxRaw)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": errors.New("invalid txhash")})
+		return
+	}
+
+	mdRaw, isPRVTx, _, txHash, err := extractDataFromRawTx(rawTxBytes)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	var md *bridge.BurningRequest
+	md, ok := mdRaw.(*bridge.BurningRequest)
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "invalid metadata type"})
+		return
+	}
+	var burnTokenInfo *wcommon.TokenInfo
+	var burntAmount uint64
+	isUnifiedToken := false
+	networkList := []string{}
+	tokenID := ""
+	uTokenID := ""
+	externalAddr := ""
+	returnOTA := ""
+	if md != nil {
+		burnTokenInfo, err = getTokenInfo(md.TokenID.String())
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": errors.New("not supported token")})
+			return
+		}
+		tokenID = burnTokenInfo.TokenID
+		uTokenID = burnTokenInfo.TokenID
+		burntAmount = md.BurningAmount
+		externalAddr = md.RemoteAddress
+		// todo: update sdk to get returnOTA
+	}
+
+	feeToken := "0000000000000000000000000000000000000000000000000000000000000004"
+	feeAmount := 0
+	pfeeAmount := 0
+
+	status, err := submitproof.SubmitUnshieldTx(txHash, []byte(req.TxRaw), isPRVTx, feeToken, uint64(feeAmount), uint64(pfeeAmount), tokenID, uTokenID, burntAmount, isUnifiedToken, externalAddr, networkList, returnOTA, "", userAgent)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"Result": map[string]interface{}{"inc_request_tx_status": status}})
+
+	return
 	//}
 }
 

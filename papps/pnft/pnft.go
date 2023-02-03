3b91173bd456da310f6dc5dda2909f6d85f3df94
Incognito Developer <develop@incognito.org>
2023-02-03 15:44:08 +0700

    update ulr

--- papps/pnft/pnft.go
+++ papps/pnft/pnft.go
@@ -124,26 +124,27 @@ func RetrieveGetNftListFromMoralis(APIEndpoint, token, address string) (string, 
 		PageSize int         `json:"page_size"`
 		Cursor   interface{} `json:"cursor"`
 		Result   []struct {
-			TokenAddress      string    `json:"token_address"`
-			TokenID           string    `json:"token_id"`
-			OwnerOf           string    `json:"owner_of"`
-			BlockNumber       string    `json:"block_number"`
-			BlockNumberMinted string    `json:"block_number_minted"`
-			TokenHash         string    `json:"token_hash"`
-			Amount            string    `json:"amount"`
-			ContractType      string    `json:"contract_type"`
-			Name              string    `json:"name"`
-			Symbol            string    `json:"symbol"`
-			TokenURI          string    `json:"token_uri"`
-			Metadata          string    `json:"metadata"`
-			LastTokenURISync  time.Time `json:"last_token_uri_sync"`
-			LastMetadataSync  time.Time `json:"last_metadata_sync"`
-			MinterAddress     string    `json:"minter_address"`
+			TokenAddress       string      `json:"token_address"`
+			TokenID            string      `json:"token_id"`
+			OwnerOf            string      `json:"owner_of"`
+			BlockNumber        string      `json:"block_number"`
+			BlockNumberMinted  string      `json:"block_number_minted"`
+			TokenHash          string      `json:"token_hash"`
+			Amount             string      `json:"amount"`
+			ContractType       string      `json:"contract_type"`
+			Name               string      `json:"name"`
+			Symbol             string      `json:"symbol"`
+			TokenURI           string      `json:"token_uri"`
+			Metadata           string      `json:"metadata"`
+			NormalizedMetadata interface{} `json:"normalized_metadata"`
+			LastTokenURISync   time.Time   `json:"last_token_uri_sync"`
+			LastMetadataSync   time.Time   `json:"last_metadata_sync"`
+			MinterAddress      string      `json:"minter_address"`
 		} `json:"result"`
 		Status string `json:"status"`
 	}
 
-	url := fmt.Sprintf("%s/%s/nft?chain=eth&format=decimal", APIEndpoint, address)
+	url := fmt.Sprintf("%s/%s/nft?chain=eth&format=decimal&normalizeMetadata=true&limit=100", APIEndpoint, address)
 
 	log.Println("url: ", url)
 
